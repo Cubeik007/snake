f8d8422af236bf70cdff43cc74c47b2f20d46741
Your Name <speedcubeik@gmail.com>
2022-05-26 21:06:16 +0200

    new changees

--- main.py
+++ main.py
@@ -4,39 +4,48 @@ from pygame.locals import *
 from random import randint
 
 SIZE = 30
-TIME = 0.2
+TIME = 0.3
 START_X = 250
 START_Y = 250
 LENGTH = 3
+FIELD_SIZE = 30*20
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((500, 500))
+        self.surface = pygame.display.set_mode((FIELD_SIZE, FIELD_SIZE))
+        self.pause_image = pygame.image.load("images/Pause_image-removebg-preview.png").convert_alpha()
+        self.gameover_image = pygame.image.load("images/Game_over-removebg-preview.png").convert_alpha()
         self.surface.fill((85, 235, 52))
         self.snake = Snake(self.surface, LENGTH)
         self.apple = Apple(self.surface)
         self.snake.draw()
         self.apple.draw()
+        self.time = TIME
 
     def next_move(self):
         self.snake.draw()
         self.apple.draw()
 
     def check_colision(self):
-        if abs(self.snake.x[0] -  self.apple.x) < SIZE and abs(self.snake.y[0] -  self.apple.y) < SIZE:
+        print(self.snake.x[0], self.apple.x, self.snake.y[0], self.apple.y)
+        if self.snake.x[0] == self.apple.x and self.snake.y[0] ==  self.apple.y:
             self.apple.change_apple()
             self.snake.append_block()
+            self.time *= 0.9
             # print(self.check_if_apple_valid_position())
             while not self.check_if_apple_valid_position():
                 self.apple.change_apple()
 
     def pause(self):
+        self.surface.blit(self.pause_image, (20, 20))
+        pygame.display.update()
         while True:
             for event in pygame.event.get():
                 if event.type == QUIT:
                     pygame.quit()
                 if event.type == KEYDOWN and event.key == K_p:
+                    self.surface.fill((85, 235, 52))
                     return True
     
     def check_if_apple_valid_position(self):
@@ -45,6 +54,18 @@ class Game:
             if self.snake.x[i] == self.apple.x and self.snake.y[i] == self.apple.y:
                 return False
         return True
+
+    def game_over(self):
+        self.surface.blit(self.gameover_image, (20, 20))
+        pygame.display.update()
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                if event.type == KEYDOWN and event.key == K_r:
+                    self.surface.fill((85, 235, 52))
+                    return True
+
 
     def run(self):
         running = True
@@ -71,8 +92,10 @@ class Game:
                     running = False
             #print(self.snake.direction)
             time.sleep(TIME)
+            """if self.snake.check_illegal_move:
+                self.game_over()"""
             self.check_colision()
-            self.snake.set_direction()  
+            self.snake.set_direction()
             self.next_move()
             
 
@@ -87,8 +110,7 @@ class Snake():
         self.block = pygame.transform.scale(self.block, (SIZE, SIZE))
         self.x = [START_X-30*k for k in range(length)]
         self.y = [START_Y]*length
-        self.direction = None
-        self.all_directions = ["right"]*length
+        self.direction = "right"
         self.speed = SIZE
 
     def draw(self):
@@ -98,16 +120,20 @@ class Snake():
         pygame.display.update()
     
     def move_right(self):
-        self.x[0] += self.speed
+        self.x[0] += self.speed 
+        self.x[0] %= FIELD_SIZE
 
     def move_left(self):
-        self.x[0] -= self.speed
+        self.x[0] -= self.speed 
+        self.x[0] %= FIELD_SIZE
 
     def move_up(self):
-        self.y[0] -= self.speed
+        self.y[0] -= self.speed 
+        self.y[0] %= FIELD_SIZE
 
     def move_down(self):
         self.y[0] += self.speed
+        self.y[0] %= FIELD_SIZE
 
     def set_direction(self):
         for block_id in reversed(range(self.length)):
@@ -128,7 +154,10 @@ class Snake():
         self.length += 1
         self.x.append(self.x[-1])
         self.y.append(self.y[-1])
-        self.all_directions.append(self.all_directions[-1])
+
+    def check_illegal_move(self):
+        for i in range(1, self.length):
+            return self.x[0] == self.x[i] and self.y[0] == self.y[i]
 
 class Apple():
     def __init__(self, parent_screen):
